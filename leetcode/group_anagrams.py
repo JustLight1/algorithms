169c7a610acab0c5f4343b434b7327bd2054b9db
Форов Александр <5h9t5@list.ru>
2023-10-19 18:24:34 +0300

    add countPairs

---
+++ leetcode/group_anagrams.py
@@ -0,0 +1,16 @@
+"""
+Given a 0-indexed integer array nums of length n and an integer target, return 
+the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+"""
+
+from typing import List
+
+
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] < target:
+                    count += 1
+        return count
