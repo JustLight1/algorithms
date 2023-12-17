80e163d92f77352f12fd0bcd8620638ff487e276
Форов Александр <5h9t5@list.ru>
2023-12-17 18:45:20 +0300

    fix flake8

--- leetcode/decompress_RLElist.py
+++ leetcode/decompress_RLElist.py
@@ -1,8 +1,7 @@
 def decompressRLElist(nums):
-    res = []
     for i in range(len(nums)):
-        b, c = [nums[2*i], nums[2*(i+1)]]
+        b, c = [nums[2 * i], nums[2 * (i + 1)]]
     print(b, c)
 
 
-decompressRLElist([1,2,3,4])
+decompressRLElist([1, 2, 3, 4])
