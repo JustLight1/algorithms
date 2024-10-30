66fce08bb0ea6e0c1ce7b22619cad6d6560eea79
Forov Alexandr <5h9t5@list.ru>
2024-10-31 01:03:39 +0300

    Update jewels_and_stones.py

--- leetcode/jewels_and_stones.py
+++ leetcode/jewels_and_stones.py
@@ -4,8 +4,8 @@ and stones representing the stones you have. Each character in stones is a type
 of stone you have. You want to know how many of the stones you have are also
 jewels.
 
-Letters are case sensitive, so "a" is considered a different type of stone from
-"A".
+Letters are case sensitive, so "a" is considered a different type of stone 
+from "A".
 
 Example 1:
 
